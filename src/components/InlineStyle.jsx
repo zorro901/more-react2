30fda6aafa593f278daaf053d2bfa8a6461b588a
zorro901 <zoro907@gmail.com>
2021-08-08 17:33:21 +0900

    12. Inline Styles

--- src/components/InlineStyle.jsx
+++ src/components/InlineStyle.jsx
@@ -1,8 +1,28 @@
 export const InlineStyle = () => {
+  const containerStyle = {
+    border: "solid 2px #392eff",
+    borderRadius: "20px",
+    padding: "8px",
+    margin: "8px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center"
+  };
+  const titleStyle = {
+    margin: 0,
+    color: "3d84a8"
+  };
+  const buttonStyle = {
+    backgroundColor: "#abedd8",
+    border: "none",
+    padding: "8px",
+    borderRadius: "8px"
+  };
+
   return (
-    <div>
-      <p>- Inline Styles -</p>
-      <button>FIGHT!</button>
+    <div style={containerStyle}>
+      <p style={titleStyle}>- Inline Styles -</p>
+      <button style={buttonStyle}>FIGHT!</button>
     </div>
-  )
-}
+  );
+};
