5a96eb29e83bc14d451286da0119ed5178111bb2
zorro901 <zoro907@gmail.com>
2021-08-08 18:01:02 +0900

    15. styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/InlineStyle";
+import { StyledComponent } from "./components/StyledComponents";
 import { StyledJsx } from "./components/StyledJsx";
 import "./styles.css";
 
@@ -9,6 +10,7 @@ export default function App() {
       <InlineStyle />
       <CssModules />
       <StyledJsx />
+      <StyledComponent />
     </div>
   );
 }
