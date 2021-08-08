cface48256dcd0bdf678c74909f533d3e6fab9ee
zorro901 <zoro907@gmail.com>
2021-08-08 17:26:37 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { InlineStyle } from "./components/InlineStyle";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+    </div>
+  );
+}
